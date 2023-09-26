cf40e788beb4ef5ea73e3b6b97f8031cc0cb094a
Steven H <smhanson21@gmail.com>
2023-09-25 19:28:57 -0500

    install bcrypt^5.0.0

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -44,7 +44,7 @@ router.post('/login', async (req, res) => {
     }
 });
 
-// Still need logout.js functionality
+// Still need logout.js functionality to get
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
         req.session.destroy(() => {
