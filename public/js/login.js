3d0d528a44a45c536bb032115827f399a51d09f7
Steven H <smhanson21@gmail.com>
2023-09-26 20:26:41 -0500

    remove comment

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,6 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
 
     if (email && password) {
-        // Send a POST request to the API endpoint
         const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
