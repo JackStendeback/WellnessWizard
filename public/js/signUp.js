fc3783c58f908474c28db16d864cf165705805e1
Emily Caton <caton.emily@yahoo.com>
2023-09-28 19:49:52 -0500

    update signUp.js

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,25 +1,31 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    const confirmPassword = document.querySelector('#confirm-password').value.trim();
 
-    if (name && email && password) {
+    if (username && email && password && confirmPassword) {
+        if (password !== confirmPassword) {
+            alert("Password does not match");
+            return;
+        }
+
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
             document.location.replace('/');
         } else {
-            alert(response.statusText);
+            alert("Failed to sign up. Please check your input.");
         }
     }
 };
 
 document
-    .querySelector('.signup-form')
+    .querySelector('#signup-form')
     .addEventListener('submit', signupFormHandler);
